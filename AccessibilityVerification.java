e5ca50b5a38baca4b0a44cace90340c9f466b71c
vaishali031993 <165470013+vaishali031993@users.noreply.github.com>
2024-04-27 15:21:48 +0530

    Update AccessibilityVerification.java

--- AccessibilityVerification.java
+++ AccessibilityVerification.java
@@ -15,7 +15,7 @@ public class AccessibilityVerification {
         driver.get("https://magento.softwaretestingboard.com/");
         Thread.sleep(2000);
 
-        // Test case 17:Verify screen reader compatibility
+
         
         WebElement element = driver.findElement(By.tagName("body"));
         boolean screenReaderCompatible = element.isEnabled() && element.isDisplayed();
@@ -25,14 +25,12 @@ public class AccessibilityVerification {
         } else {
             System.out.println("Website is not screen reader compatible.");
         }
-
-        //Test Case 18: Verify keyboard navigation
        
         WebElement searchBox = driver.findElement(By.id("search"));
         searchBox.sendKeys("Test");
         Thread.sleep(2000);
 
-        //Test Case 19: Verify that essential functions are accessible to users with disabilities
+
       
         WebElement searchButton = driver.findElement(By.cssSelector(".actions [type='submit']"));
         searchButton.click();
